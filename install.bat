1dddeab346d168c10948b3407f275a83760768bd
Pixxth <61689690+Pixxth@users.noreply.github.com>
2020-06-16 04:31:15 -0400

    Add files via upload

---
+++ install.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Lie Modules Installer
+py -3.8 -m pip install -r requirements.txt
+echo Finished!
+pause
---
+++ launch.bat
@@ -0,0 +1,6 @@
+@echo off
+cls
+title Lie Launcher
+py -3.8 LIE.py
+echo SHIT IDK WHY NOT WORKING
+pause
